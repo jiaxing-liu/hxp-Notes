7d2ed5016288678a4cb407a45fbc40ad6eed386f
hxp <hxp201406@gmail.com>
2021-01-17 09:38:15 +0800

    modified:   7z-file-encryption/encrypt.sh

--- Computer-Engineering/7z-file-encryption/encrypt.sh
+++ Computer-Engineering/7z-file-encryption/encrypt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 TARGET_DIR=$1
-RANDOM_STR=$(head /dev/random | tr -dc '[:alnum:]' | head -c 64)
+RANDOM_STR=$(head /dev/random | tr -dc '[:alnum:]' | head -c 1024)
 
-echo ${RANDOM_STR} > ./"${TARGET_DIR}.txt"
-7z a -p"${RANDOM_STR}" -mhe ${TARGET_DIR}.7z ${TARGET_DIR}
-mkdir -p encrypted
-mv ${TARGET_DIR} encrypted/${TARGET_DIR}
+mkdir -p encrypting
+mv ${TARGET_DIR} encrypting/${TARGET_DIR}
+echo -n ${RANDOM_STR} > ./${TARGET_DIR}/"${TARGET_DIR}.txt"
+7z a -mx9 -myx9 -v1G -p"${RANDOM_STR}" -mhe ${TARGET_DIR}/${TARGET_DIR}.7z encrypting/${TARGET_DIR}
+mv encrypting encrypted
