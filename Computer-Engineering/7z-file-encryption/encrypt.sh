dc81e4d30b8389d9b6ab481752b42b64452de31b
hxp <hxp201406@gmail.com>
2021-01-22 13:37:55 +0800

    new file:   Computer-Engineering/crack-rar-password.md

--- Computer-Engineering/7z-file-encryption/encrypt.sh
+++ Computer-Engineering/7z-file-encryption/encrypt.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 TARGET_DIR=$1
-RANDOM_STR=$(head /dev/random | tr -dc '[:alnum:]' | head -c 1024)
+RANDOM_STR=$(head /dev/random | tr -dc '[:alnum:]' | head -c 64)
+echo $RANDOM_STR
 
 mkdir -p encrypting
 mv ${TARGET_DIR} encrypting/${TARGET_DIR}
-echo -n ${RANDOM_STR} > ./${TARGET_DIR}/"${TARGET_DIR}.txt"
-7z a -mx9 -myx9 -v1G -p"${RANDOM_STR}" -mhe ${TARGET_DIR}/${TARGET_DIR}.7z encrypting/${TARGET_DIR}
+mkdir ${TARGET_DIR}
+echo ${RANDOM_STR} > ./${TARGET_DIR}/"${TARGET_DIR}.txt"
+7z a -p"${RANDOM_STR}" ./${TARGET_DIR}/${TARGET_DIR}.zip ./encrypting/${TARGET_DIR}
 mv encrypting encrypted
---
+++ Computer-Engineering/crack-rar-password.md
@@ -0,0 +1,16 @@
+# Crack Multi-file large rar password
+
+## Step 0: Prerequisites
+
+Compile john:
+
+Download source code from <https://github.com/openwall/john>, unzip and complie:
+
+```bash
+unzip john-bleeding-jumbo.zip
+cd john-bleeding-jumbo
+cd src
+./configure
+make -j${nproc}
+```
+
