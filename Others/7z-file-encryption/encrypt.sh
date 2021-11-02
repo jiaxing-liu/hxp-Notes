73b936377f136afdc00ec08d7945c912332f62a0
hxp <hxp@hxp.plus>
2021-11-02 14:46:08 +0800

    modified:   Others/7z-file-encryption/encrypt.sh

--- Others/7z-file-encryption/encrypt.sh
+++ Others/7z-file-encryption/encrypt.sh
@@ -8,5 +8,5 @@ mkdir -p encrypting
 mv ${TARGET_DIR} encrypting/${TARGET_DIR}
 mkdir ${TARGET_DIR}
 echo ${RANDOM_STR} > ./${TARGET_DIR}/"${TARGET_DIR}.txt"
-7z a -p"${RANDOM_STR}" ./${TARGET_DIR}/${TARGET_DIR}.zip ./encrypting/${TARGET_DIR}
+7z a -p"${RANDOM_STR}" -mx=9 -mhe=on ./${TARGET_DIR}/${TARGET_DIR}.7z ./encrypting/${TARGET_DIR}
 mv encrypting encrypted
