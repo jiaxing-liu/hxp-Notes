fb356adcf86232c6b648e941562c06b8063bea97
hxp <hxp@hxp.plus>
2021-11-03 13:43:28 +0800

    Added quotes

--- Others/7z-file-encryption/encrypt.sh
+++ Others/7z-file-encryption/encrypt.sh
@@ -5,8 +5,8 @@ RANDOM_STR=$(head /dev/random | tr -dc '[:alnum:]' | head -c 64)
 echo $RANDOM_STR
 
 mkdir -p encrypting
-mv ${TARGET_DIR} encrypting/${TARGET_DIR}
-mkdir ${TARGET_DIR}
-echo ${RANDOM_STR} > ./${TARGET_DIR}/"${TARGET_DIR}.txt"
-7z a -p"${RANDOM_STR}" -mx=9 -mhe=on ./${TARGET_DIR}/${TARGET_DIR}.7z ./encrypting/${TARGET_DIR}
+mv "${TARGET_DIR}" "encrypting/${TARGET_DIR}"
+mkdir "${TARGET_DIR}"
+echo "${RANDOM_STR}" > "./${TARGET_DIR}/${TARGET_DIR}.txt"
+7z a -p"${RANDOM_STR}" -mx=9 -mhe=on "./${TARGET_DIR}/${TARGET_DIR}.7z" "./encrypting/${TARGET_DIR}"
 mv encrypting encrypted
