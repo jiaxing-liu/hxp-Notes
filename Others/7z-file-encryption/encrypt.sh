5906fd4f19410ba4b9a42b982337ce3a9579a790
hxp <hxp@hxp.plus>
2021-11-07 12:34:24 +0800

    Encrypt.sh: Added date

--- Others/7z-file-encryption/encrypt.sh
+++ Others/7z-file-encryption/encrypt.sh
@@ -10,3 +10,4 @@ mkdir "${TARGET_DIR}"
 echo "${RANDOM_STR}" > "./${TARGET_DIR}/${TARGET_DIR}.txt"
 7z a -p"${RANDOM_STR}" -mx=9 -mhe=on "./${TARGET_DIR}/${TARGET_DIR}.7z" "./encrypting/${TARGET_DIR}"
 mv encrypting encrypted
+mv "${TARGET_DIR}" "${TARGET_DIR}_$(date +%Y%m%d%H%M%S)"
